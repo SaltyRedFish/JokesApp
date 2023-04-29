3210d5ab520152fe4de834bdd2a99489b7e7563e
jarm3l2e <fkor3@AMK.cpq>
2023-04-29 12:01:30 +0800

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by T Krobot on 29/4/23.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
